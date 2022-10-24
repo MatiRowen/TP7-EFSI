53ffa9018a0ca6d0959d34589ea8926e6c0539a3
Android Studio <devandroid@ort>
2022-10-24 08:36:04 -0300

    24/10

--- src/components/ListaProducts.js
+++ src/components/ListaProducts.js
@@ -28,10 +28,18 @@ export default function Home() {
       {query.map( 
         (Product)=>(
           <>
+          <div class="container text-center">
+          <div class="row row-cols-1  g-4 mb-5">
+            <div id="card-e" class=" m-5 pb-3 pt-3">
             <div key={Product.id} onClick={() => navigate(`/Detalle/${Product.id}`)}>
+              <center>
             <p className="text-xl">{Product.title}</p>
-            <img src={Product.images[0]}/>
-            </div>
+            <img className="product-tam" src={Product.images[0]}/>
+            </center>
+          </div>
+          </div>
+          </div>
+          </div>
           </>
         )
       )}
